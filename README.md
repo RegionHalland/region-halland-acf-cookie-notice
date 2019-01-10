966a6295dce7106271aad8e024a315137fbb8fbb
Hydén Roland RGS VS SYSTEMUTV IT <rhn014@lthalland.se>
2019-01-10 16:23:01 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# region-halland-acf-cookie-notice
-Front-end-plugin för GDPR-funktion (kallat cookie-notice)
+# Skapa en "cookie notice"
+
+## Hur man använder Region Hallands plugin "region-halland-acf-cookie-notice"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-cookie-notice".
+
+
+## Användningsområde
+
+Denna plugin skapar funktionalitet för en "cookie notice", dvs:
+
+```sh
+A) Lägger till två fält i databasen. Ett fält för informationstext + ett fält för knapptext
+B) Skapar ett formulär under "Temainställningar" i Wp-admin där man kan editera texterna
+C) En funktion för att hämta ut respektive text som en array
+D) En funktion för att kontrollera om en cookie är satt eller inte
+```
+
+OBS! Denna plugin förutsätter att du har installerat och aktiverat Advanced Custom Fields Pro (https://www.advancedcustomfields.com/pro/)
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-cookie-notice.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-cookie-notice.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-cookie-notice": "1.0.0"
+},
+```
+
+
+## Visa "cookie notice" på en sida via "Blade"
+
+```sh
+@if(function_exists('check_region_halland_cookie_notice'))
+  @php($checkCookieNotice = check_region_halland_cookie_notice())
+  @if ($checkCookieNotice == false)
+    @php($myCookieNotice = get_region_halland_cookie_notice())  
+    <span class="h5">{!! $myCookieNotice['message'] !!}</span>
+    <button id="cookie-consent" class="btn btn-primary">
+      {!! $myCookieNotice['button'] !!}
+    </button>
+  @endif
+@endif
+```
+
+
+## Exempel på hur arrayen kan se ut
+
+```sh
+array (size=2)
+  'message' => string 'På den här webbplatsen använder vi cookies (kakor) för att webbplatsen ska fungera på ett bra sätt för dig. Genom att klicka vidare eller på ”Jag förstår” godkänner du att vi använder cookies.' (length=208)
+  'button' => string 'Jag förstår' (length=1
+```
+
+
+## Jquery för att kontrollera ifall användaren klickar på knappen
+
+```sh
+$("#cookie-consent").on( "click", function() {
+    
+    // set cookie with javascript function
+    setCookie('cookie_notice_accepted','1',365);
+    
+    // Hide div with cookie notice text + button
+    $("#cookie-notice").hide();
+
+});
+```
+
+
+## Javascript för att skapa cookien
+
+```sh
+function setCookie(name,value,days) {
+    
+    // Set variables
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    
+    // Set cookie
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+
+}
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_cookie_notice.php
@@ -0,0 +1,164 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Cookie Notice
+	 */
+	/*
+	Plugin Name: Region Halland ACF Cookie Notice
+	Description: ACF-fält för text till en cookie notice
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'my_acf_add_cookie_notice_field_groups');
+
+	// Lägg till ett formulär
+	function my_acf_add_cookie_notice_field_groups() {
+
+		// Om funktionen existerar
+		if (function_exists('acf_add_local_field_group')):
+
+			// Lägg till formulär via acf
+			acf_add_local_field_group(array(
+				'key' => 'group_5b3239313bbe6',
+				'title' => 'Hantera text för cookies',
+				'fields' => array(
+					array(
+						'key' => 'field_5b323d0d20e3b',
+						'label' => 'Cookies',
+						'name' => '',
+						'type' => 'tab',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'placement' => 'top',
+						'endpoint' => 0,
+					),
+					array(
+						'key' => 'field_5b323e3a33ca1',
+						'label' => '',
+						'name' => 'cookie',
+						'type' => 'group',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'layout' => 'block',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_5b323d3820e3d',
+								'label' => 'Meddelande',
+								'name' => 'message',
+								'type' => 'textarea',
+								'instructions' => 'Fyll i information om att kakor används',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => 'På den här webbplatsen använder vi cookies (kakor) för att webbplatsen ska fungera på ett bra sätt för dig. Genom att klicka vidare eller på ”Jag förstår” godkänner du att vi använder cookies.',
+								'placeholder' => '',
+								'maxlength' => '',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+								'key' => 'field_5b323d8620e3e',
+								'label' => 'Knappttext',
+								'name' => 'button',
+								'type' => 'text',
+								'instructions' => 'Fyll i knapptext för godkännande av användning av kakor',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => 'Jag förstår',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+								'maxlength' => '',
+							),
+						),
+					)
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'options_page',
+							'operator' => '==',
+							'value' => 'acf-options-temainstallningar',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => 1,
+				'description' => '',
+			));
+
+		endif;
+
+	}
+
+	// Kolla om det finns någon cookie
+	function check_region_halland_cookie_notice()
+	{
+		// Namnet på cookien
+		$cookieName = 'cookie_notice_accepted';
+
+		// returnera true/false
+		if (!isset($_COOKIE[$cookieName])) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	// Hämta ACF-texter för cokkie-notice
+	function get_region_halland_cookie_notice()
+	{
+		// Hämta respektive värde från databasen
+		$cookie_notice['message'] = get_field('cookie_message', 'options');
+		$cookie_notice['button'] = get_field('cookie_button', 'options');
+		
+		// Returnera värdena som en array
+		return $cookie_notice;
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_cookie_notice_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_cookie_notice_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_cookie_notice_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_cookie_notice_deactivate');
+?>
